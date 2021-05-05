1a71a67c68ec8c86f545c3060c3442048c8cb3c6
Davi Cruz <47984117+davicruzof@users.noreply.github.com>
2021-05-05 08:51:00 -0300

    feat: Firebase UI
    tela prontas como os metodos de login

--- 4 - Authentication/js/app.js
+++ 4 - Authentication/js/app.js
@@ -6,3 +6,27 @@ function logout() {
       alert('usuario deslogou');
     });
 }
+
+/**
+ * Espera o evento de que a DOM está pronta para executar algo
+ */
+document.addEventListener('DOMContentLoaded', function () {
+  var ui = new firebaseui.auth.AuthUI(firebase.auth());
+
+  var config = {
+    callbacks: {
+      signInSuccessWithAuthResult: function (authResult) {
+        console.log('🚀 ~ authResult ~', authResult);
+        return false;
+      },
+    },
+    signInOptions: [
+      firebase.auth.EmailAuthProvider.PROVIDER_ID,
+      firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      firebase.auth.GithubAuthProvider.PROVIDER_ID,
+    ],
+    signInFlow: 'popup',
+  };
+  ui.start('#firebaseui-auth', config);
+});
