e4c53df781c2d679e8366c53b5e27121b30222c1
Davi Cruz <47984117+davicruzof@users.noreply.github.com>
2021-05-05 06:59:44 -0300

    feat: Auth - Google
    Autenticação com o google

--- 4 - Authentication/js/google.js
+++ 4 - Authentication/js/google.js
@@ -1,3 +1,14 @@
 function loginGoogle() {
+  let provider = new firebase.auth.GoogleAuthProvider();
 
+  firebase
+    .auth()
+    .signInWithPopup(provider)
+    .then((response) => {
+      console.log('🚀 User:', response.user);
+      console.log('🚀 Token:', response.credential.accessToken);
+    })
+    .catch((err) => {
+      console.log('🚀 ~ Erro login do facebook', err);
+    });
 }
